7e2e532a1f65301827b89ee386f3c832d9a51fb2
saif-py <mohdsaifsiddiqui10@gmail.com>
2021-02-02 02:23:48 +0530

    added voice

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from subprocess import call
 import webbrowser
 import pyautogui
 import time
+import sys
+import pyttsx3
+from gtts import gTTS
 
 listt = []
 
@@ -22,11 +25,25 @@ def OpenApplication(listt):
                 call(i[1])
 
 
+def say(line):
+    engine = pyttsx3.init()
+    # rate of speech
+    newVoiceRate = 160
+    engine.setProperty('rate', newVoiceRate)
+    voices = engine.getProperty('voices')
+    # engine.setProperty('voice', voices[17].id)
+    engine.setProperty('voice', 'english+f16')
+    # testing
+    engine.say(line)
+    engine.runAndWait()
+
+
 r = sr.Recognizer()
+say("hello! I'm U.B! the virtual assistant! how can i help you")
 while True:
     with sr.Microphone() as source:
 
-        print("listening...")
+        print("listening....")
         audio = r.record(source, duration=3)
         try:
             str = r.recognize_google(audio)
@@ -44,7 +61,6 @@ while True:
         webbrowser.open('https://www.google.com.tr/search?q=' + search.strip())
     elif listt[0].lower() == 'what' or listt[0].lower() == 'who' or listt[0].lower() == 'when' or \
             listt[0].lower() == 'how' or listt[0].lower() == 'is':
-        import webbrowser
 
         search = ''
         for a in listt:
@@ -54,24 +70,51 @@ while True:
 
     elif listt[0].lower() == 'open':
         OpenApplication(listt)
-    elif listt[0].lower() == 'play':
-        from googlesearch import search
+    elif listt[0].lower() == 'hello' or listt[0].lower() == 'hi' or listt[0].lower() == 'hai':
+        mytext = 'hello! i am UB'
+        say(mytext)
+        language = 'en'
 
-        query = ''
-        for a in listt:
-            query = query + ' ' + a
-        for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-            k = j.split('/')
-            if k[2] == 'www.amazon.com' or k[2] == 'www.netflix.com' or k[2] == 'www.primevideo.com':
-                webbrowser.get('firefox').open_new_tab(j)
-                # webbrowser.open(j)
-                # OpenApplication(['open', 'firefox'])
-                # # time.sleep(2)
-                # # pyautogui.write(j)
-                # pyautogui.hotkey('enter')
-                break
-    # elif listt[0].lower() == "shutdown":
-        # call("shutdown now")      
+        myobj = gTTS(text=mytext, lang=language, slow=False)
+
+        myobj.save("welcome.mp3")
+        os.system("welcome.mp3")
+
+    elif listt[0].lower() == 'play':
+        if listt[1].lower() == 'music':
+            say("opening music player")
+            import sys
+
+            sys.path.insert(0, '/home/saif/PycharmProjects/music_player')
+            import music_player
+            music_player()
+        else:
+
+            from googlesearch import search
+
+            query = ''
+            for a in listt:
+                query = query + ' ' + a
+            for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+                k = j.split('/')
+                if k[2] == 'www.amazon.com' or k[2] == 'www.netflix.com' or k[2] == 'www.primevideo.com':
+                    webbrowser.get('firefox').open_new_tab(j)
+
+                    break
+                if k[2] == 'www.youtube.com':
+                    webbrowser.open_new_tab(j)
+                    break
+
+    elif listt[0].lower() == 'exit' or listt[0].lower() == 'quit':
+        say('good byee...')
+        quit()
     else:
         command = os.popen(str).readlines()
-        print(command)
+        if command.__contains__('not found'):
+            search = ''
+            for a in listt:
+                search = search + ' ' + a
+
+            webbrowser.open('https://www.google.com.tr/search?q=' + search.strip())
+        else:
+            print(command)
